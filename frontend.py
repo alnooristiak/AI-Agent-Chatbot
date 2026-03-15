ed5add7c9f29ff49478f0406f77468dc0fd9d3d5
Al Noor Istiak Mahmud <istiakmahmud1010@gmail.com>
2026-03-15 22:03:58 +0600

    initialize Streamlit frontend UI with basic layout and system prompt input

---
+++ frontend.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+
+# Setup UI with streamlit
+import streamlit as st
+
+st.set_page_config(page_title="LangGraph Agent UI", layout="centered")
+st.title("AI Chatbot Agents")
+st.write("Create and Interact with the AI Agents!")
+
+system_prompt=st.text_area("Define your AI Agent: ", height=70, placeholder="Type your system prompt here...")
