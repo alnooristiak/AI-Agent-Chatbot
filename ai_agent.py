db8c0f9bbba319748de26964fe5855809d686913
Al Noor Istiak Mahmud <istiakmahmud1010@gmail.com>
2026-03-16 02:33:02 +0600

    langchain server error

--- ai_agent.py
+++ ai_agent.py
@@ -27,7 +27,8 @@ def get_response_from_ai_agent(llm_id, query, allow_search, system_prompt, provi
     agent = create_react_agent(
         model=llm,
         tools=tools,
-        state_modifier=system_prompt
+        prompt=system_prompt
+        # state_modifier=system_prompt
     )
     
     
